b92812cf7cd10fb0cdebcb64ae4b71d3a12bf395
thehouseplant <sean.collins@outlook.com>
2020-06-01 21:37:51 -0700

    Remove unused includes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-import Redlands from '../components/redlands'
+import Redlands from "../components/redlands"
 
 const IndexPage = () => (
   <Layout>
