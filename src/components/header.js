8ed2f803a0f2430ba69149fd105a119172f85e8f
thehouseplant <sean.collins@outlook.com>
2020-06-02 01:21:58 -0700

    Add pages to site navigation

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,47 @@ import PropTypes from "prop-types"
 import React from "react"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
+import NavDropdown from "react-bootstrap/NavDropdown"
 
 const Header = ({ siteTitle }) => (
   <Navbar bg="dark" variant="dark" expand="lg">
-    <Navbar.Brand href="#">{siteTitle}</Navbar.Brand>
+    <Link to="/">
+      <Navbar.Brand as="span">{siteTitle}</Navbar.Brand>
+    </Link>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Link to="/">
+          <Nav.Link as="span">Home</Nav.Link>
+        </Link>
+        <Link to="/cities">
+          <Nav.Link as="span">Cities</Nav.Link>
+        </Link>
+        <Link to="/counties">
+          <Nav.Link as="span">Counties</Nav.Link>
+        </Link>
+        <NavDropdown title="Education" id="basic-nav-dropdown">
+          <Link to="/universities">
+            <NavDropdown.Item as="span">Universities</NavDropdown.Item>
+          </Link>
+          <Link to="/community-colleges">
+            <NavDropdown.Item as="span">Community Colleges</NavDropdown.Item>
+          </Link>
+          <Link to="/k-12">
+            <NavDropdown.Item as="span">K-12 Schools</NavDropdown.Item>
+          </Link>
+          <Link to="/charter">
+            <NavDropdown.Item as="span">Charter Schools</NavDropdown.Item>
+          </Link>
+        </NavDropdown>
+        <Link to="/special">
+          <Nav.Link as="span">Special Districts</Nav.Link>
+        </Link>
+        <Link to="/state">
+          <Nav.Link as="span">State of California</Nav.Link>
+        </Link>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 )
 
