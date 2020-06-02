4e6674acc6bd18581cf95e159662b0ee919e9258
thehouseplant <sean.collins@outlook.com>
2020-06-02 07:56:57 -0700

    Add breadcrumb navigation

--- src/pages/cities.js
+++ src/pages/cities.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,6 +8,16 @@ import SEO from "../components/seo"
 const CitiesPage = () => (
   <Layout>
     <SEO title="Cities" />
+
+    <Breadcrumb>
+      <Breadcrumb.Item>
+        <Link to="/">Home</Link>
+      </Breadcrumb.Item>
+      <Breadcrumb.Item active>
+        Cities
+      </Breadcrumb.Item>
+    </Breadcrumb>
+    
     <h1>Cities</h1>
 
     <Link to="/cities/redlands">Redlands</Link>
