bfe6d9d5f66322bb6a22303c91c4b46462dd085b
thehouseplant <sean.collins@outlook.com>
2020-06-02 19:53:11 -0700

    Remove unused imports

--- src/components/table.js
+++ src/components/table.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useTable, usePagination, useSortBy } from 'react-table'
 import BTable from 'react-bootstrap/Table'
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Button from 'react-bootstrap/Button'
 import Pagination from 'react-bootstrap/Pagination'
 import Row from 'react-bootstrap/Row'
